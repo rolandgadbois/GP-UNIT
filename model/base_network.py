f4bd1aa0791980e84065dc0cfda317b9c5aed052
rolandgadbois <roland@nochet.org>
2025-07-29 13:51:33 -0700

    Update base_network.py

--- model/base_network.py
+++ model/base_network.py
@@ -53,22 +53,3 @@ class BaseNetwork(nn.Module):
             if hasattr(m, 'init_weights'):
                 m.init_weights(init_type, gain)
                 
-                
-class AdaptiveInstanceNorm(BaseNetwork):
-    def __init__(self, style_dim, in_channel):
-        super(AdaptiveInstanceNorm, self).__init__()
-
-        self.norm = nn.InstanceNorm2d(in_channel)
-        self.style = nn.Linear(style_dim, in_channel * 2)
-
-        self.style.bias.data[:in_channel] = 1
-        self.style.bias.data[in_channel:] = 0
-
-    def forward(self, input, style):
-        style = self.style(style).unsqueeze(2).unsqueeze(3)
-        gamma, beta = style.chunk(2, 1)
-
-        out = self.norm(input)
-        out = gamma * out + beta
-
-        return out
