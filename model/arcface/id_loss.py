a5c2db96f26d98c3b349f74f09390ecee172f31f
rolandgadbois <roland@nochet.org>
2025-07-29 14:20:13 -0700

    Update id_loss.py

--- model/arcface/id_loss.py
+++ model/arcface/id_loss.py
@@ -1,39 +1,34 @@
 import torch
 from torch import nn
 
-from model.arcface.model_irse import Backbone
+from model.simclr import SimCLR
 
 
 class IDLoss(nn.Module):
-    def __init__(self, path):
+    def __init__(self, checkpoint_path):
         super(IDLoss, self).__init__()
-        print('Loading ResNet ArcFace')
-        self.facenet = Backbone(input_size=112, num_layers=50, drop_ratio=0.6, mode='ir_se')
-        self.facenet.load_state_dict(torch.load(path))
+        print('Loading custom SimCLR encoder')
+        self.encoder = SimCLR(checkpoint_path)
         self.pool = torch.nn.AdaptiveAvgPool2d((256, 256))
         self.face_pool = torch.nn.AdaptiveAvgPool2d((112, 112))
-        self.facenet.eval()
-        #self.opts = opts
+        self.encoder.eval()
 
     def extract_feats(self, x):
         if x.shape[2] != 256:
             x = self.pool(x)
-        x = x[:, :, 35:223, 32:220]  # Crop interesting region
+        x = x[:, :, 35:223, 32:220]  # Crop region like ArcFace
         x = self.face_pool(x)
-        x_feats = self.facenet(x)
+        x_feats = self.encoder(x)
         return x_feats
 
     def forward(self, y_hat, y):
         n_samples = y.shape[0]
-        y_feats = self.extract_feats(y)  # Otherwise use the feature from there
+        y_feats = self.extract_feats(y)
         y_hat_feats = self.extract_feats(y_hat)
         y_feats = y_feats.detach()
-        loss = 0
-        #sim_improvement = 0
-        count = 0
-        for i in range(n_samples):
-            diff_target = y_hat_feats[i].dot(y_feats[i])
-            loss += 1 - diff_target
-            count += 1
 
-        return loss / count
+        loss = 0
+        for i in range(n_samples):
+            sim = torch.dot(y_hat_feats[i], y_feats[i])
+            loss += 1 - sim
+        return loss / n_samples
